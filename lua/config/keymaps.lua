d47baf4119dbc713f5ec7cc30dc4b8431694c67d
jhivandb <jhivanb@gmail.com>
2024-05-25 01:54:12 +0530

    Added catpuccin, quit to ctrl+q

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- quit file
+local map = vim.keymap.set
+map({ "i", "x", "n", "s" }, "<C-q>", "<cmd>q<cr><esc>", { desc = "Quit File" })
